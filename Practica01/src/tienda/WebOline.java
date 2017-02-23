016a93bfa96d0057933562acf423077af29e9ee8
Chupendo <andresruizpenuela@gmail.com>
2017-02-23 18:24:26 +0100

    ewqrweq

--- Practica01/src/tienda/WebOline.java
+++ Practica01/src/tienda/WebOline.java
@@ -20,28 +20,32 @@ public class WebOline extends HttpServlet
 			throws ServletException, IOException 
 	{
 		//Variables
-		String url =""; //Para ir cambiando las vista.
+		String url; //Para ir cambiando las vista.
 		System.out.println("ProbandoooO.");
 		//Leemos parametros
 		String id_user = request.getParameter("name");
 		String id_key = request.getParameter("key");
-		System.out.println("ProbandoooO.");
 		System.out.println("name= "+id_user+" key= "+id_key);
+		url="/user.jsp";
+		/*
 		if(request.getParameter("name")==null || request.getParameter("key")==null )
 		{//Se a cargado la web por primera vez.
 			url=web[0];
+			System.out.println(url);
 		}
 		else
 		{
 			if (id_user.equals(admin[0]) && id_key.equals(admin[1]))
 			{//Los datos introducidos coincide con el administrado.
-				url=web[2];					
+				url=web[2];
+				System.out.println(url);
 			}
 			else
 			{//Los datos introducidos son del usuario.
 				url=web[1]; 
+				System.out.println(url);
 			}
-		}
+		}*/
 		getServletContext().getRequestDispatcher(url).forward(request, response);	
 
 	}
